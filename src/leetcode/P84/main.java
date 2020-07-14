22efb9461530cfb7c18c884b39619e49a081056c
jmDevBlog <systemdata227@gmail.com>
2020-07-14 17:33:03 +0900

    IntelliJ Commit

---
+++ src/leetcode/P84/main.java
@@ -0,0 +1,12 @@
+package leetcode.P84;
+
+public class main {
+    public static void main(String[] args) {
+        int[] heights = {2,1,5,6,2,3};
+
+        Solution s = new Solution();
+
+        System.out.println(s.largestRectangle(heights));
+    }
+
+}
