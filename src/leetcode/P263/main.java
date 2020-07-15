8a040f252d122d7c147f81a68e33d8c45c214fe6
jmDevBlog <systemdata227@gmail.com>
2020-07-15 18:32:19 +0900

    P263. Ugly Number

---
+++ src/leetcode/P263/main.java
@@ -0,0 +1,10 @@
+package leetcode.P263;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.isUgly(6));
+        System.out.println(s.isUgly(8));
+        System.out.println(s.isUgly(14));
+    }
+}
