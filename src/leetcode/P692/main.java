d3d6f0336fde8e0f2cbe281ee05c14dcdaf0f7b8
jmDevBlog <systemdata227@gmail.com>
2020-07-16 22:07:44 +0900

    P692. Top K Frequent Words

---
+++ src/leetcode/P692/main.java
@@ -0,0 +1,11 @@
+package leetcode.P692;
+
+public class main {
+    public static void main(String[] args) {
+        String[] words = {"i", "love", "leetcode", "i", "love", "coding"};
+
+        Solution s = new Solution();
+
+        System.out.println(s.topKFrequent(words, 2));
+    }
+}
