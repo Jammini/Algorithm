52db8833486296a9ec4d54eb09afa3755dbd604f
jmDevBlog <systemdata227@gmail.com>
2020-07-23 21:40:04 +0900

    P485. Max Consecutive Ones

---
+++ src/leetcode/P485/Solution.java
@@ -0,0 +1,23 @@
+package leetcode.P485;
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int answer=0;
+        int cnt=0;
+
+        for(int i=0; i< nums.length; i++){
+            if(nums[i]==1){
+                cnt++;
+            }else{
+                if(answer < cnt){
+                    answer = cnt;
+                }
+                cnt=0;
+            }
+        }
+        if(answer < cnt){
+            answer = cnt;
+        }
+        return answer;
+    }
+}
