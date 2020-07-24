1bb8675fe5a83ced091d9486a2c0a19e658d2646
jmDevBlog <systemdata227@gmail.com>
2020-07-24 20:18:23 +0900

    P1470. Shuffle the Array

---
+++ src/leetcode/P1470/main.java
@@ -0,0 +1,12 @@
+package leetcode.P1470;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] num = {2, 5, 1, 3, 4, 7};
+        int n = 3;
+
+        s.shuffle(num, n);
+    }
+}
