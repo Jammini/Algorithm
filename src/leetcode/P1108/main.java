b754e76ea205ef194044a8267d573cacdc302904
jmDevBlog <systemdata227@gmail.com>
2020-07-26 01:07:07 +0900

    P1108. Defanging an IP Address

---
+++ src/leetcode/P1108/main.java
@@ -0,0 +1,9 @@
+package leetcode.P1108;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.defangIPaddr("1.1.1.1"));
+    }
+}
