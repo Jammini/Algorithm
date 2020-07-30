dc8d1570ebd2ca7bc1aa2b0e143fbbbdcb9abf4e
jmDevBlog <systemdata227@gmail.com>
2020-07-30 17:28:10 +0900

    P1528. Shuffle String

---
+++ src/leetcode/P1528/main.java
@@ -0,0 +1,10 @@
+package leetcode.P1528;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] indices = {4, 5, 6, 7, 0, 2, 1, 3};
+        System.out.println(s.restoreString("codeleet",indices));
+    }
+}
