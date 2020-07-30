14dfd4d71978c60ad9840956471e3c05c8211ee6
jmDevBlog <systemdata227@gmail.com>
2020-07-30 23:50:02 +0900

    P1365. How Many Numbers Are Smaller Than the Current Number

---
+++ src/leetcode/P1365/main.java
@@ -0,0 +1,11 @@
+package leetcode.P1365;
+
+public class main {
+    public static void main(String[] args) {
+        int nums[] = {8,1,2,2,3};
+
+        Solution s = new Solution();
+
+        s.smallerNumbersThanCurrent(nums);
+    }
+}
