98e74f8781d3afdb9baf6579140d5ef610222760
jmDevBlog <systemdata227@gmail.com>
2020-10-28 23:52:53 +0900

    P605. Can Place Flowers.

---
+++ src/leetcode/P605/main.java
@@ -0,0 +1,12 @@
+package leetcode.P605;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        int[] flowerbed ={0,0,1,0,0};
+        int n = 1;
+
+        System.out.println(s.canPlaceFlowers(flowerbed, n));
+    }
+}
