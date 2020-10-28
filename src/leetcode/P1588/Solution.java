684408511f93a799508caa601084c3c286a364e0
jmDevBlog <systemdata227@gmail.com>
2020-10-28 23:54:26 +0900

    P1588. Sum of All Odd Length Subarrays

---
+++ src/leetcode/P1588/Solution.java
@@ -0,0 +1,22 @@
+package leetcode.P1588;
+
+public class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+
+        int answer = 0;
+
+        for (int i = 1; i <= arr.length; i = i + 2) {
+            for (int j = 0; j < arr.length; j++) {
+                int end = i+j;
+                if (end > arr.length){
+                    end = arr.length;
+                    break;
+                }
+                for (int k = j; k < end ; k++) {
+                    answer += arr[k];
+                }
+            }
+        }
+        return answer;
+    }
+}
