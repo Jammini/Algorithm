f639fc7451137bb7c05540259613468b081ee4e0
jmDevBlog <systemdata227@gmail.com>
2020-10-28 23:51:46 +0900

    P509. Fibonacci Number

---
+++ src/leetcode/P509/main.java
@@ -0,0 +1,11 @@
+package leetcode.P509;
+
+public class main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.fib(2));
+        System.out.println(s.fib(3));
+        System.out.println(s.fib(4));
+    }
+}
