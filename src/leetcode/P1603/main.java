8af9c8418804da0bc3659b2a17a864138537acb9
jmDevBlog <systemdata227@gmail.com>
2020-10-30 19:43:57 +0900

    P1603. Design Parking System

---
+++ src/leetcode/P1603/main.java
@@ -0,0 +1,11 @@
+package leetcode.P1603;
+
+public class main {
+    public static void main(String[] args) {
+        Solution obj = new Solution(1, 1, 0);
+        System.out.println(obj.addCar(1));
+        System.out.println(obj.addCar(2));
+        System.out.println(obj.addCar(3));
+        System.out.println(obj.addCar(1));
+    }
+}
