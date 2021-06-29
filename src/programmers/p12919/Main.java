3ca9818fdab5288d8073aac9e6f4bb8a2c2eb070
jammini <systemdata227@gmail.com>
2021-06-29 10:25:29 +0900

    P12919. 서울에서 김서방 찾기

---
+++ src/programmers/p12919/Main.java
@@ -0,0 +1,7 @@
+package programmers.p12919;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new String[]{"Jane", "Kim"}));
+    }
+}
