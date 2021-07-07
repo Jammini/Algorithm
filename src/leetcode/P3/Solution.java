861f05682b77c476e48b776ceadd34ddb0f87023
jmDevBlog <systemdata227@gmail.com>
2021-07-07 22:37:20 +0900

    p3. Longest Substring Without Repeating Characters (성능 개선 필요)

--- src/leetcode/P3/Solution.java
+++ src/leetcode/P3/Solution.java
@@ -4,13 +4,29 @@ import java.util.HashSet;
 
 public class Solution {
     public int lengthOfLongestSubstring(String s) {
+        int answer = 1;
         HashSet set = new HashSet();
 
-        for(int i= 0; i<s.length(); i++){
-            set.add(s.charAt(i));
+        if ("".equals(s)) {
+            return 0;
         }
-
-
-        return set.size();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (!set.contains(s.charAt(i))) {
+                set.add(s.charAt(i));
+                count++;
+            } else {
+                if (answer < count){
+                    answer = count;
+                }
+                i = i - count;
+                count = 0;
+                set.clear();
+            }
+        }
+        if (answer < count){
+            answer = count;
+        }
+        return answer;
     }
 }
--- src/leetcode/P3/main.java
+++ src/leetcode/P3/main.java
@@ -3,6 +3,12 @@ package leetcode.P3;
 public class main {
     public static void main(String[] args) {
         Solution s = new Solution();
+        System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
+        System.out.println(s.lengthOfLongestSubstring("bbbbb"));
         System.out.println(s.lengthOfLongestSubstring("pwwkew"));
+        System.out.println(s.lengthOfLongestSubstring(""));
+        System.out.println(s.lengthOfLongestSubstring(" "));
+        System.out.println(s.lengthOfLongestSubstring("au"));
+        System.out.println(s.lengthOfLongestSubstring("aab"));
     }
 }
