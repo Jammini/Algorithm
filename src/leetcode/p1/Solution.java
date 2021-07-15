a9e22e1cf6d76d979685b7fc0ba5747d1d01c2e9
jmDevBlog <systemdata227@gmail.com>
2021-07-15 23:31:27 +0900

    P1. Two Sum

---
+++ src/leetcode/p1/Solution.java
@@ -0,0 +1,19 @@
+package leetcode.p1;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] answer = new int[2];
+
+        out:
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    answer[0] = i;
+                    answer[1] = j;
+                    break out;
+                }
+            }
+        }
+        return answer;
+    }
+}
