cf30f33b57bb44a502bd332279832b604344b7d9
jmDevBlog <systemdata227@gmail.com>
2021-08-16 19:15:50 +0900

    p12969. 직사각형 별찍기

---
+++ src/programmers/p12969/Main.java
@@ -0,0 +1,13 @@
+package programmers.p12969;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        new Solution().solution(a, b);
+    }
+}
---
+++ src/programmers/p12969/Solution.java
@@ -0,0 +1,12 @@
+package programmers.p12969;
+
+public class Solution {
+    public void solution(int a, int b) {
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
