b426df8889c00f0678fe39607a4fe9229c0ff143
jmDevBlog <systemdata227@gmail.com>
2021-08-19 01:39:41 +0900

    p76501. 음양더하기

---
+++ src/programmers/p76501/Main.java
@@ -0,0 +1,8 @@
+package programmers.p76501;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{4, 7, 12}, new boolean[]{true, false, true}));
+        System.out.println(new Solution().solution(new int[]{1, 2, 3}, new boolean[]{false, false, true}));
+    }
+}
