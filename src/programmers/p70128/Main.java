a776575690d5ee89bf3505e9795218419c08629a
jmDevBlog <systemdata227@gmail.com>
2021-08-19 01:40:09 +0900

    p70128. 내적

---
+++ src/programmers/p70128/Main.java
@@ -0,0 +1,8 @@
+package programmers.p70128;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{1, 2, 3, 4}, new int[]{-3, -1, 0, 2}));
+        System.out.println(new Solution().solution(new int[]{-1, 0, 1}, new int[]{1, 0, -1}));
+    }
+}
