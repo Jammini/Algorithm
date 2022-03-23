bb8b682908c1e3cf11f05ecd5d51ddf0a2b0e0a7
Jammini <systemdata227@gmail.com>
2022-03-23 10:34:43 +0900

    2037. Minimum Number of Moves to Seat Everyone

---
+++ src/leetcode/p2037/Main.java
@@ -0,0 +1,10 @@
+package leetcode.p2037;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.minMovesToSeat(new int[]{3, 1, 5}, new int[]{2, 7, 4}));
+        System.out.println(s.minMovesToSeat(new int[]{4, 1, 5, 9}, new int[]{1, 3, 2, 6}));
+        System.out.println(s.minMovesToSeat(new int[]{2, 2, 6, 6}, new int[]{1, 3, 2, 6}));
+    }
+}
---
+++ src/leetcode/p2037/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.p2037;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        int answer = 0;
+        Arrays.sort(seats);
+        Arrays.sort(students);
+
+        for (int i = 0; i < seats.length; i++) {
+            answer += Math.abs(seats[i] - students[i]);
+        }
+        return answer;
+    }
+}
