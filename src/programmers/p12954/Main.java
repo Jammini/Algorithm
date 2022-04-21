45e655e0579df6f334b467481e29d3147c578c4c
Jammini <systemdata227@gmail.com>
2022-04-21 10:20:07 +0900

    12954. x만큼 간격이 있는 n개의 숫자

---
+++ src/programmers/p12954/Main.java
@@ -0,0 +1,12 @@
+package programmers.p12954;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(Arrays.toString(s.solution(2, 5)));
+        System.out.println(Arrays.toString(s.solution(4, 3)));
+        System.out.println(Arrays.toString(s.solution(-4, 2)));
+    }
+}
