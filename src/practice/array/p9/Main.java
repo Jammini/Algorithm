ee8acf921e57e011b7bd30f0c8c9fbd6eec8e29b
Jammini <systemdata227@gmail.com>
2022-06-27 16:46:41 +0900

    9. 격자판 최대합

---
+++ src/practice/array/p9/Main.java
@@ -0,0 +1,39 @@
+package practice.array.p9;
+
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int n, int[][] arr) {
+        int answer = Integer.MIN_VALUE;
+        int sum1, sum2;
+        for (int i = 0; i < n; i++) {
+            sum1 = sum2 = 0;
+            for (int j = 0; j < n; j++) {
+                sum1 += arr[i][j];
+                sum2 += arr[j][i];
+            }
+            answer = Math.max(answer, Math.max(sum1, sum2));
+        }
+        sum1 = sum2 = 0;
+        for (int i = 0; i < n; i++) {
+            sum1 += arr[i][i];
+            sum2 += arr[n - 1 - i][n - 1 - i];
+        }
+        answer = Math.max(answer, Math.max(sum1, sum2));
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[][] arr = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        System.out.print(T.solution(n, arr));
+    }
+}
