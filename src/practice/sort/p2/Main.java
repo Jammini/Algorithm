97f3df8a43ee490b543d51f50d3840480f6d1bb3
Jammini <systemdata227@gmail.com>
2022-07-26 18:11:53 +0900

    2. 버블정렬

---
+++ src/practice/sort/p2/Main.java
@@ -0,0 +1,33 @@
+package practice.sort.p2;
+
+import java.util.Scanner;
+
+public class Main {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = arr;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt(); // 6
+        int[] arr = new int[n]; // 13 5 11 7 23 15
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
