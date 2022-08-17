6d927099d85c84c93c02e075719b8907003dc2a3
Jammini <systemdata227@gmail.com>
2022-08-17 17:51:52 +0900

    Algorithm-19 올바른 괄호

---
+++ src/programmers/p12909/Main.java
@@ -0,0 +1,10 @@
+package programmers.p12909;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("()()")); // true
+        System.out.println(new Solution().solution("(())()")); // true
+        System.out.println(new Solution().solution(")()(")); // false
+        System.out.println(new Solution().solution("(()(")); // false
+    }
+}
