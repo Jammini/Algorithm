441bfd710a91882920c2d7faad8470937db62dcf
Jammini <systemdata227@gmail.com>
2022-09-06 14:48:35 +0900

    완주하지 못한 선수 재풀이

--- src/programmers/p42576/Main.java
+++ src/programmers/p42576/Main.java
@@ -2,8 +2,11 @@ package programmers.p42576;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(new String[]{"leo", "kiki", "eden"},new String[]{"eden", "kiki"}));
-        System.out.println(new Solution().solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"},new String[]{"josipa", "filipa", "marina", "nikola"}));
-        System.out.println(new Solution().solution(new String[]{"mislav", "stanko", "mislav", "ana"},new String[]{"stanko", "ana", "mislav"}));
+        System.out.println(new Solution().solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        System.out.println(new Solution2().solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}));
+        System.out.println(new Solution().solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        System.out.println(new Solution2().solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}));
+        System.out.println(new Solution().solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
+        System.out.println(new Solution2().solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}));
     }
 }
