68641c754bb9e204161d5293de73108a1b91b1f6
Jammini <systemdata227@gmail.com>
2022-09-08 09:52:13 +0900

    Algorithm-34 약수의 개수와 덧셈

---
+++ src/programmers/p77884/Main.java
@@ -0,0 +1,8 @@
+package programmers.p77884;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(13, 17)); // 43
+        System.out.println(new Solution().solution(24, 27)); // 52
+    }
+}
---
+++ src/programmers/p77884/Solution.java
@@ -0,0 +1,22 @@
+package programmers.p77884;
+
+public class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for (int i = left; i <= right; i++) {
+            int count = 1;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count % 2 == 1) {
+                answer -= i;
+            } else {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
