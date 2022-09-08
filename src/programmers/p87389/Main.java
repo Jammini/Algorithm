4dc92b8c26f35af31eb2ec94129e9c51c12b124e
Jammini <systemdata227@gmail.com>
2022-09-08 10:34:44 +0900

    Algorithm-35 나머지가 1이 되는 수 찾기

---
+++ src/programmers/p87389/Main.java
@@ -0,0 +1,8 @@
+package programmers.p87389;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(10)); // 3
+        System.out.println(new Solution().solution(12)); // 11
+    }
+}
---
+++ src/programmers/p87389/Solution.java
@@ -0,0 +1,14 @@
+package programmers.p87389;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 1) {
+                return i;
+            }
+        }
+        return answer;
+    }
+}
