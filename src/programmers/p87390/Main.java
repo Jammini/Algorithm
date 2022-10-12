8d42861fd12ddb7ec63ae07257ce71019b29e500
Jammini <systemdata227@gmail.com>
2022-10-12 16:35:55 +0900

    Algorithm-81 n^2 배열 자르기

---
+++ src/programmers/p87390/Main.java
@@ -0,0 +1,11 @@
+package programmers.p87390;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().solution(3, 2, 5))); // [3,2,2,3]
+        System.out.println(Arrays.toString(new Solution().solution(4, 7, 14))); // [4,3,3,3,4,4,4,4]
+        System.out.println(Arrays.toString(new Solution().solution(5, 18, 23))); //
+    }
+}
