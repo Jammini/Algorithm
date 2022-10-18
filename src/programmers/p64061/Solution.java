9c84df283c3c78c9e7ef8181e2c5a80f93ecb081
Jammini <systemdata227@gmail.com>
2022-10-18 13:43:46 +0900

    Algorithm-76 크레인 인형뽑기 게임

---
+++ src/programmers/p64061/Solution.java
@@ -0,0 +1,26 @@
+package programmers.p64061;
+
+import java.util.Stack;
+
+public class Solution {
+    public int solution(int[][] board, int[] moves) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int move : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][move - 1] > 0) {
+                    if (!stack.isEmpty() && stack.peek() == board[i][move - 1]) {
+                        stack.pop();
+                        answer = answer + 2;
+                    } else {
+                        stack.push(board[i][move - 1]);
+                    }
+                    board[i][move - 1] = 0;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
