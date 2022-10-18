58f7ae63310453a200968c895860216e84fe9cb3
Jammini <systemdata227@gmail.com>
2022-10-18 10:14:27 +0900

    Algorithm-92 전화번호 목록

---
+++ src/programmers/p42577/Main.java
@@ -0,0 +1,11 @@
+package programmers.p42577;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new String[]{"119", "97674223", "1195524421"})); // false
+        System.out.println(new Solution().solution(new String[]{"123", "456", "789"})); // false
+        System.out.println(new Solution().solution(new String[]{"12", "123", "1235", "567", "88"})); // false
+        System.out.println(new Solution().solution(new String[]{"12", "1123", "1235", "567", "88"})); // false
+    }
+}
