ccad8d4c1fae9cf151e441ab27e7817d8616028e
Jammini <systemdata227@gmail.com>
2022-10-31 10:29:18 +0900

    Algorithm-119 방문 길이

---
+++ src/programmers/p49994/Main.java
@@ -0,0 +1,8 @@
+package programmers.p49994;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("ULURRDLLU")); // 7
+        System.out.println(new Solution().solution("LULLLLLLU")); // 7
+    }
+}
