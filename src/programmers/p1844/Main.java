a701bd537cdf99497038fd3aa7d510ee907eb383
Jammini <systemdata227@gmail.com>
2022-11-04 10:27:21 +0900

    Algorithm-125 게임 맵 최단거리

---
+++ src/programmers/p1844/Main.java
@@ -0,0 +1,9 @@
+package programmers.p1844;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}})); // 11
+        System.out.println(s.solution(new int[][]{{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 0}, {0, 0, 0, 0, 1}})); // -1
+    }
+}
