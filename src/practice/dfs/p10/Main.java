c8691f82fc93109b502e6cf75aa931acd29bd970
jammini <systemdata227@gmail.com>
2022-11-07 18:03:25 +0900

    11. 미로의 최단거리 통로(BFS)

--- src/practice/dfs/p10/Main.java
+++ src/practice/dfs/p10/Main.java
@@ -1,25 +1,39 @@
 package practice.dfs.p10;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
+
+class Point {
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
 
 public class Main {
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
-    static int[][] board;
+    static int[][] board, dis;
     static int answer = 0;
 
     public void dfs(int x, int y) {
-        if (x == 7 && y == 7) {
-            answer++;
-        } else {
+        Queue<Point> queue = new LinkedList<>();
+        queue.offer(new Point(x, y));
+        board[x][y] = 1;
+        while (!queue.isEmpty()) {
+            Point tmp = queue.poll();
             for (int i = 0; i < dx.length; i++) {
-                if (x + dx[i] >= 1 && x + dx[i] <= 7 && y + dy[i] >= 1 && y + dy[i] <= 7 && board[x + dx[i]][y + dy[i]] == 0) {
-                    board[x + dx[i]][y + dy[i]] = 1;
-                    dfs(x + dx[i], y + dy[i]);
-                    board[x + dx[i]][y + dy[i]] = 0;
+                if (tmp.x + dx[i] >= 1 && tmp.x + dx[i] <= 7 && tmp.y + dy[i] >= 1 && tmp.y + dy[i] <= 7 && board[tmp.x + dx[i]][tmp.y + dy[i]] == 0) {
+                    board[tmp.x + dx[i]][tmp.y + dy[i]] = 1;
+                    queue.offer(new Point(tmp.x + dx[i], tmp.y + dy[i]));
+                    dis[tmp.x + dx[i]][tmp.y + dy[i]] = dis[tmp.x][tmp.y] + 1;
                 }
+                answer++;
             }
-
         }
     }
 
@@ -27,6 +41,7 @@ public class Main {
         Main T = new Main();
         Scanner kb = new Scanner(System.in);
         board = new int[8][8];
+        dis = new int[8][8];
 
         for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 7; j++) {
@@ -35,6 +50,11 @@ public class Main {
         }
         board[1][1] = 1;
         T.dfs(1, 1);
+        if (dis[7][7] == 0) {
+            answer = -1;
+        } else {
+            answer = dis[7][7];
+        }
         System.out.println(answer);
     }
 }
