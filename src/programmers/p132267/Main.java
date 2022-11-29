23edae9ef7ffbe436c6c982bfa27f59593b71efa
Jammini <systemdata227@gmail.com>
2022-11-29 10:16:52 +0900

    Algorithm-159 콜라 문제

---
+++ src/programmers/p132267/Main.java
@@ -0,0 +1,8 @@
+package programmers.p132267;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(2, 1, 20)); // 19
+        System.out.println(new Solution().solution(3, 1, 20)); // 9
+    }
+}
