39cc61b4d78943476fabc8d28999e7146094981e
Jammini <systemdata227@gmail.com>
2022-11-29 11:11:45 +0900

    Algorithm-160 삼총사

---
+++ src/programmers/p131705/Main.java
@@ -0,0 +1,9 @@
+package programmers.p131705;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{-2, 3, 0, 2, -5})); // 2
+        System.out.println(new Solution().solution(new int[]{-3, -2, -1, 0, 1, 2, 3})); // 5
+        System.out.println(new Solution().solution(new int[]{-1, 1, -1, 1})); // 0
+    }
+}
