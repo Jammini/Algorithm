286eb34c8d4a8ac92e690282e0f8b9817141ad77
Jammini <systemdata227@gmail.com>
2022-12-13 14:45:08 +0900

    Algorithm-192 최대공약수와 최소공배수

---
+++ src/programmers/p12940/Main.java
@@ -0,0 +1,10 @@
+package programmers.p12940;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().solution(3, 12))); // [3, 12]
+        System.out.println(Arrays.toString(new Solution().solution(2, 5))); // [1, 10]
+    }
+}
