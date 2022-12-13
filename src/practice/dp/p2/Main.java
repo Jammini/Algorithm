07008bdcc4e1cc98c877a3b7f5d95853b4ff9e50
Jammini <systemdata227@gmail.com>
2022-12-13 20:10:47 +0900

    2. 돌다리 건너기

---
+++ src/practice/dp/p2/Main.java
@@ -0,0 +1,24 @@
+package practice.dp.p2;
+
+import java.util.Scanner;
+
+public class Main {
+    static int[] dy;
+
+    public int solution(int n) {
+        dy[1] = 1;
+        dy[2] = 2;
+        for (int i = 3; i <= n + 1; i++) {
+            dy[i] = dy[i - 1] + dy[i - 2];
+        }
+        return dy[n + 1];
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        dy = new int[n + 2];
+        System.out.println(T.solution(n));
+    }
+}
