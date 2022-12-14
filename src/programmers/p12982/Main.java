e557fc1fe6ad8e8f898b7773c37d78431c281f2b
Jammini <systemdata227@gmail.com>
2022-12-14 20:43:05 +0900

    Algorithm-194 예산

---
+++ src/programmers/p12982/Main.java
@@ -0,0 +1,8 @@
+package programmers.p12982;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{1, 3, 2, 5, 4}, 9)); // 3
+        System.out.println(new Solution().solution(new int[]{2, 2, 3, 3}, 10)); // 4
+    }
+}
