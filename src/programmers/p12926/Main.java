4f478ef869ca3ede129f35b3b875b99df43b048a
Jammini <systemdata227@gmail.com>
2022-12-15 18:35:18 +0900

    Algorithm-196 시저 암호

---
+++ src/programmers/p12926/Main.java
@@ -0,0 +1,10 @@
+package programmers.p12926;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("AB", 1)); // "BC"
+        System.out.println(new Solution().solution("z", 1)); // "a"
+        System.out.println(new Solution().solution("Z", 25)); // "a"
+        System.out.println(new Solution().solution("a B z", 4)); // "e F d"
+    }
+}
