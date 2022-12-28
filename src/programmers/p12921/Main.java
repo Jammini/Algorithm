ee919dec3a8ab781be7f39c573e4ca0d02b62506
Jammini <systemdata227@gmail.com>
2022-12-28 17:58:00 +0900

    Algorithm-211 소수 찾기

---
+++ src/programmers/p12921/Main.java
@@ -0,0 +1,8 @@
+package programmers.p12921;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(10)); // 4
+        System.out.println(new Solution().solution(5)); // 3
+    }
+}
