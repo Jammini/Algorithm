1c3029b393efe4ff96e7104e0beea6f948884303
Jammini <systemdata227@gmail.com>
2023-01-05 18:25:22 +0900

    Algorithm-218 크기가 작은 부분 문자열

---
+++ src/programmers/p147355/Main.java
@@ -0,0 +1,9 @@
+package programmers.p147355;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("3141592", "271")); // 2
+        System.out.println(new Solution().solution("500220839878", "7")); // 8
+        System.out.println(new Solution().solution("10203", "15")); // 3
+    }
+}
