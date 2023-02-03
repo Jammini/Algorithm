782c2265e18907477d9e603c342a3b7c920623f3
김정민 <systemdata227@gmail.com>
2023-02-03 21:39:00 +0900

    Algorithm-239 롤케이크 자르기

---
+++ src/programmers/p132265/Main.java
@@ -0,0 +1,8 @@
+package programmers.p132265;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[]{1, 2, 1, 3, 1, 4, 1, 2})); // 2
+        System.out.println(new Solution().solution(new int[]{1, 2, 3, 1, 4})); // 0
+    }
+}
