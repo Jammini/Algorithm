4abb47b7cb74a31ad42d1c409a99c0ccd4dd140e
김정민 <systemdata227@gmail.com>
2023-03-02 23:57:20 +0900

    Algorithm-253 477. Total Hamming Distance

---
+++ src/leetcode/p477/Main.java
@@ -0,0 +1,11 @@
+package leetcode.p477;
+
+/**
+ * https://leetcode.com/problems/total-hamming-distance/description/
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().totalHammingDistance(new int[]{4, 14, 2})); // 6
+        System.out.println(new Solution().totalHammingDistance(new int[]{4, 14, 4})); // 4
+    }
+}
