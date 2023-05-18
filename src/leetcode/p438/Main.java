7cbb91138e70cd7955d77e4c69958ed98b76c0bd
jeongmin.kim <systemdata227@gmail.com>
2023-05-18 17:15:41 +0900

    Algorithm-304 438. Find All Anagrams in a String

---
+++ src/leetcode/p438/Main.java
@@ -0,0 +1,8 @@
+package leetcode.p438;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().findAnagrams("cbaebabacd", "abc")); // [0,6]
+        System.out.println(new Solution().findAnagrams("abab", "ab")); // [0,1,2]
+    }
+}
