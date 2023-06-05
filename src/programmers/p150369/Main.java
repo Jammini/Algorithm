4b63de1898a908cb8491d51fa561eda127e3bbdc
jammini <systemdata227@gmail.com>
2023-06-05 22:20:08 +0900

    Algorithm-315 택배 배달과 수거하기

---
+++ src/programmers/p150369/Main.java
@@ -0,0 +1,8 @@
+package programmers.p150369;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(4, 5, new int[]{1, 0, 3, 1, 2}, new int[]{0, 3, 0, 4, 0})); // 16
+        System.out.println(new Solution().solution(2, 7, new int[]{1, 0, 2, 0, 1, 0, 2}, new int[]{0, 2, 0, 1, 0, 2, 0})); // 30
+    }
+}
