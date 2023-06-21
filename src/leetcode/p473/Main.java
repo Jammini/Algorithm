9174640d95a7cf6a0ba91b422bbb1e2d7b9ab2f7
jammini <systemdata227@gmail.com>
2023-06-21 23:38:47 +0900

    Algorithm-321 473. Matchsticks to Square

---
+++ src/leetcode/p473/Main.java
@@ -0,0 +1,8 @@
+package leetcode.p473;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().makesquare(new int[]{1, 1, 2, 2, 2})); // true
+        System.out.println(new Solution().makesquare(new int[]{3, 3, 3, 3, 4})); // false
+    }
+}
