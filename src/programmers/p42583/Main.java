0471072f2b1ef116873708d3b35fef25a70ebbfb
jammini <systemdata227@gmail.com>
2024-04-18 16:52:36 +0900

    [programmer] 다리를 지나는 트럭 재풀이

--- src/programmers/p42583/Main.java
+++ src/programmers/p42583/Main.java
@@ -2,17 +2,20 @@ package programmers.p42583;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(1, 1, new int[]{1, 1, 1, 1})); // 5
-        System.out.println(new Solution().solution(2, 10, new int[]{7, 4, 5, 6})); // 8
-        System.out.println(new Solution().solution(3, 10, new int[]{7, 4, 5, 6})); // 11
-        System.out.println(new Solution().solution(3, 10, new int[]{1, 2, 3, 2, 1, 2, 3})); // 10
-        System.out.println(new Solution().solution(3, 10, new int[]{1, 1, 1, 1, 1, 1, 1})); // 10
-        System.out.println(new Solution().solution(3, 5, new int[]{2, 2, 2, 2, 2, 2, 2})); // 13
-        System.out.println(new Solution().solution(3, 5, new int[]{1, 1, 1, 1, 1, 1, 1})); // 10
-        System.out.println(new Solution().solution(7, 5, new int[]{1, 1, 1, 1, 1, 1, 1})); // 16
-        System.out.println(new Solution().solution(4, 10, new int[]{7, 4, 5, 6})); // 14
-        System.out.println(new Solution().solution(4, 100, new int[]{7, 4, 5, 6})); // 8
-        System.out.println(new Solution().solution(100, 100, new int[]{10})); // 101
-        System.out.println(new Solution().solution(100, 50, new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10})); // 204
+//        System.out.println(new Solution().solution(1, 1, new int[]{1, 1, 1, 1})); // 5
+//        System.out.println(new Solution2().solution(1, 1, new int[]{1, 1, 1, 1})); // 5
+//        System.out.println(new Solution().solution(2, 10, new int[]{7, 4, 5, 6})); // 8
+        System.out.println(new Solution2().solution(2, 10, new int[]{7, 4, 5, 6})); // 8
+//        System.out.println(new Solution().solution(3, 10, new int[]{7, 4, 5, 6})); // 11
+//        System.out.println(new Solution().solution(3, 10, new int[]{1, 2, 3, 2, 1, 2, 3})); // 10
+//        System.out.println(new Solution().solution(3, 10, new int[]{1, 1, 1, 1, 1, 1, 1})); // 10
+//        System.out.println(new Solution().solution(3, 5, new int[]{2, 2, 2, 2, 2, 2, 2})); // 13
+//        System.out.println(new Solution().solution(3, 5, new int[]{1, 1, 1, 1, 1, 1, 1})); // 10
+//        System.out.println(new Solution().solution(7, 5, new int[]{1, 1, 1, 1, 1, 1, 1})); // 16
+//        System.out.println(new Solution().solution(4, 10, new int[]{7, 4, 5, 6})); // 14
+//        System.out.println(new Solution().solution(4, 100, new int[]{7, 4, 5, 6})); // 8
+//        System.out.println(new Solution().solution(100, 100, new int[]{10})); // 101
+//        System.out.println(new Solution2().solution(100, 100, new int[]{10})); // 101
+//        System.out.println(new Solution().solution(100, 50, new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10})); // 204
     }
 }
