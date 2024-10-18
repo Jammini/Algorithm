425e910f76087cbb5c6dfec8a216222a1d50e1db
Jammini <systemdata227@gmail.com>
2024-10-19 02:08:10 +0900

    N과 M (3) 풀이

---
+++ src/practice/dfs/nm/p3/NM3.java
@@ -0,0 +1,30 @@
+package practice.dfs.nm.p3;
+
+public class NM3 {
+    int[] arr;
+
+    public void Solution(int N, int M) {
+        arr = new int[M];
+        dfs(N, M,  0);
+    }
+
+    private void dfs(int N, int M, int L) {
+        if (L == M) {
+            for (int i : arr) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            arr[L] = i + 1;
+            dfs(N, M, L + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        new NM3().Solution(3, 1);
+        new NM3().Solution(4, 2);
+        new NM3().Solution(4, 4);
+    }
+}
