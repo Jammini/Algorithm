0be7e177844a84de107d44e3720b91e3d8b2fb2b
Jammini <systemdata227@gmail.com>
2025-09-08 23:19:28 +0900

    [leetcode] 2178. Maximum Split of Positive Even Integers

--- src/leetcode/p2178/Main.java
+++ src/leetcode/p2178/Main.java
@@ -1,11 +1,10 @@
 package leetcode.p2178;
 
-import java.util.Arrays;
-
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().maximumEvenSplit(new int[]{3, 1, -2, -5, 2, -4}))); // [3,-2,1,-5,2,-4]
-        System.out.println(Arrays.toString(new Solution().maximumEvenSplit(new int[]{-1, 1}))); // [1,-1]
+        System.out.println(new Solution().maximumEvenSplit(12L));
+        System.out.println(new Solution().maximumEvenSplit(7L));
+        System.out.println(new Solution().maximumEvenSplit(28L));
     }
 }
