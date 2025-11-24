e4077024ab3eb3169072855ba6059c0bd369406d
Jammini <systemdata227@gmail.com>
2025-11-24 13:50:23 +0900

    [leetcode] 3191. Minimum Operations to Make Binary Array Elements Equal to One I

---
+++ src/leetcode/p3191/Main.java
@@ -0,0 +1,9 @@
+package leetcode.p3191;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minOperations(new int[]{0, 1, 1, 1, 0, 0})); // 3
+        System.out.println(new Solution().minOperations(new int[]{0, 1, 1, 1})); // -1
+    }
+}
