8bc06ea202dafd651622a41e50a2adc1010d1036
Jammini <systemdata227@gmail.com>
2025-12-06 22:58:42 +0900

    [leetcode] 1894. Find the Student that Will Replace the Chalk

---
+++ src/leetcode/p1894/Main.java
@@ -0,0 +1,9 @@
+package leetcode.p1894;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().chalkReplacer(new int[]{5, 1, 5}, 22));
+        System.out.println(new Solution().chalkReplacer(new int[]{3, 4, 1, 2}, 25));
+    }
+}
---
+++ src/leetcode/p1894/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.p1894;
+
+public class Solution {
+
+    public int chalkReplacer(int[] chalk, int k) {
+        long totalChalk = 0;
+        for (int c : chalk) {
+            totalChalk += c;
+        }
+
+        k = (int) (k % totalChalk);
+
+        for (int i = 0; i < chalk.length; i++) {
+            if (k < chalk[i]) {
+                return i;
+            }
+            k -= chalk[i];
+        }
+        return -1;
+    }
+}
